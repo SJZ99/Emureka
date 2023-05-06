8261a1b48a25a9bbaf4bf6031566ec3c448fc2d1
nike951847 <sos7mi@gmail.com>
2023-05-06 15:14:19 +0800

    改顏色

--- app/src/main/java/com/emureka/serialandbluetooth/ui/theme/Theme.kt
+++ app/src/main/java/com/emureka/serialandbluetooth/ui/theme/Theme.kt
@@ -13,8 +13,8 @@ private val DarkColorPalette = darkColors(
 )
 
 private val LightColorPalette = lightColors(
-    primary = Emu,
-    primaryVariant = Emu2,
+    primary = Emu_light,
+    primaryVariant = Emu2_light,
     secondary = Grass
 
     /* Other default colors to override
